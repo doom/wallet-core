dd14cbb1a47195495ee67cacb62362cd04504e32
Clément "Doom" Doumergue <clement.doumergue@epitech.eu>
2022-09-04 16:09:06 +0200

    Generate Aptos coin stub

---
+++ tests/Aptos/TWCoinTypeTests.cpp
@@ -0,0 +1,34 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here MAY BE LOST.
+// Generated one-time (codegen/bin/cointests)
+//
+
+#include "../interface/TWTestUtilities.h"
+#include <TrustWalletCore/TWCoinTypeConfiguration.h>
+#include <gtest/gtest.h>
+
+TEST(TWAptosCoinType, TWCoinType) {
+    const auto coin = TWCoinTypeAptos;
+    const auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(coin));
+    const auto id = WRAPS(TWCoinTypeConfigurationGetID(coin));
+    const auto name = WRAPS(TWCoinTypeConfigurationGetName(coin));
+    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("5646"));
+    const auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(coin, txId.get()));
+    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0xf3f2d2bc98bf666bc4869f5ff56723e5c9d252a63209bb2377ea64cd1a41aec0"));
+    const auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(coin, accId.get()));
+
+    assertStringsEqual(id, "aptos");
+    assertStringsEqual(name, "Aptos");
+    assertStringsEqual(symbol, "APTOS");
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 8);
+    ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainAptos);
+    ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0x0);
+    ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0x0);
+    assertStringsEqual(txUrl, "https://explorer.devnet.aptos.dev/txn/5646");
+    assertStringsEqual(accUrl, "https://explorer.devnet.aptos.dev/account/0xf3f2d2bc98bf666bc4869f5ff56723e5c9d252a63209bb2377ea64cd1a41aec0");
+}
